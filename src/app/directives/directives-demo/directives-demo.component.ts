e1c1359ff3d16479b06ac7eec468f03c377131fe
safvan-rakhda <smrakhda@gmail.com>
2023-03-16 13:05:05 +0530

    *ngFor adn ngSwitch example

--- src/app/directives/directives-demo/directives-demo.component.ts
+++ src/app/directives/directives-demo/directives-demo.component.ts
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
 })
 export class DirectivesDemoComponent {
   isVisible: boolean = true;
+  dummyArray: string[] = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    'Item 6',
+    'Item 7',
+    'Item 8',
+    'Item 9',
+    'Item 10',
+  ];
+
+  dropDownValues: { key: number; value: string }[] = [
+    { key: 1, value: 'Box 1' },
+    { key: 2, value: 'Box 2' },
+    { key: 3, value: 'Both' },
+  ];
+
+  selectedValue: string = '';
 
   toggleisVisible = () => (this.isVisible = !this.isVisible);
+
+  onDropdownSelectionChange(selected: any) {
+    this.selectedValue = selected.value;
+  }
 }
